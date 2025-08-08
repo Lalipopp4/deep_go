a2e6703857d4485b836d4fcd269308b790c26fe2
anton.glushko <anton.glushko@vk.team>
2025-08-08 23:49:26 +0300

    HW8

--- homework/errors/homework_test.go
+++ homework/errors/homework_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,24 +12,38 @@ import (
 // go test -v homework_test.go
 
 type MultiError struct {
-	// need to implement
+	errs []string
 }
 
 func (e *MultiError) Error() string {
-	// need to implement
-	return ""
+	return fmt.Sprintf("%d errors occured:\n\t* %s\n", len(e.errs), strings.Join(e.errs, "\t* "))
 }
 
 func Append(err error, errs ...error) *MultiError {
-	// need to implement
-	return nil
+	var (
+		me *MultiError
+		ok bool
+	)
+
+	if err == nil {
+		me = &MultiError{}
+	} else if me, ok = err.(*MultiError); !ok {
+		me = &MultiError{errs: []string{err.Error()}}
+	}
+
+	for _, e := range errs {
+		me.errs = append(me.errs, e.Error())
+	}
+
+	return me
 }
 
 func TestMultiError(t *testing.T) {
-	var err error
+	var err = errors.New("error 0")
 	err = Append(err, errors.New("error 1"))
 	err = Append(err, errors.New("error 2"))
+	err = Append(err, errors.New("error 3"))
 
-	expectedMessage := "2 errors occured:\n\t* error 1\t* error 2\n"
+	expectedMessage := "4 errors occured:\n\t* error 0\t* error 1\t* error 2\t* error 3\n"
 	assert.EqualError(t, err, expectedMessage)
 }
