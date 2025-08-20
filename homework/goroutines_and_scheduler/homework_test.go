61f1e8751bc967be8c2c953ff4cfcc62f4bebc09
anton.glushko <anton.glushko@vk.team>
2025-08-21 00:39:48 +0300

    HW12

--- homework/goroutines_and_scheduler/homework_test.go
+++ homework/goroutines_and_scheduler/homework_test.go
@@ -12,25 +12,76 @@ type Task struct {
 }
 
 type Scheduler struct {
-	// need to implement
+	queue []Task
 }
 
 func NewScheduler() Scheduler {
-	// need to implement
-	return Scheduler{}
+	return Scheduler{
+		queue: make([]Task, 0, 10),
+	}
+}
+
+func (s *Scheduler) max(i, j int) int {
+	if s.queue[i].Priority > s.queue[j].Priority {
+		return i
+	}
+
+	return j
+}
+
+func (s *Scheduler) siftDown(i int) {
+	l, r := i*2+1, i*2+2
+	if r < len(s.queue) {
+		if max := s.max(l, r); s.max(max, i) != i {
+			s.queue[i], s.queue[max] = s.queue[max], s.queue[i]
+			s.siftDown(max)
+		}
+	} else if l < len(s.queue) && s.queue[l].Priority > s.queue[i].Priority {
+		s.queue[i], s.queue[l] = s.queue[l], s.queue[i]
+	}
+}
+
+func (s *Scheduler) siftUp(i int) {
+	if s.queue[i].Priority > s.queue[(i-1)/2].Priority {
+		s.queue[i], s.queue[(i-1)/2] = s.queue[(i-1)/2], s.queue[i]
+		s.siftUp((i - 1) / 2)
+	}
 }
 
 func (s *Scheduler) AddTask(task Task) {
-	// need to implement
+	s.queue = append(s.queue, task)
+	s.siftUp(len(s.queue) - 1)
 }
 
 func (s *Scheduler) ChangeTaskPriority(taskID int, newPriority int) {
-	// need to implement
+	for i, task := range s.queue {
+		if task.Identifier != taskID {
+			continue
+		}
+
+		s.queue[i].Priority = newPriority
+		if s.queue[i].Priority > s.queue[(i-1)/2].Priority {
+			s.siftUp(i)
+		} else {
+			s.siftDown(i)
+		}
+
+		break
+	}
 }
 
 func (s *Scheduler) GetTask() Task {
-	// need to implement
-	return Task{}
+	if len(s.queue) == 0 {
+		return Task{}
+	}
+
+	task := s.queue[0]
+	s.queue[0], s.queue[len(s.queue)-1] = s.queue[len(s.queue)-1], s.queue[0]
+	s.queue = s.queue[:len(s.queue)-1]
+
+	s.siftDown(0)
+
+	return task
 }
 
 func TestTrace(t *testing.T) {
@@ -53,6 +104,7 @@ func TestTrace(t *testing.T) {
 	task = scheduler.GetTask()
 	assert.Equal(t, task4, task)
 
+	task1.Priority = 100
 	scheduler.ChangeTaskPriority(1, 100)
 
 	task = scheduler.GetTask()
